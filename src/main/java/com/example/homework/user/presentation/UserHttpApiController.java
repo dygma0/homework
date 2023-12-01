41791e489b073dbbedc61faf03c1750cf05b96c9
dygma0 <404err@naver.com>
2023-12-01 20:56:23 +0900

    feat: 비로그인 사용자 조회 api 추가

--- src/main/java/com/example/homework/user/presentation/UserHttpApiController.java
+++ src/main/java/com/example/homework/user/presentation/UserHttpApiController.java
@@ -1,19 +1,28 @@
-package com.example.homework.user;
+package com.example.homework.user.presentation;
 
+import com.example.homework.decorator.Authenticated;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-@RequestMapping("/api/users")
 @RestController
+@RequestMapping("/api/users")
 public class UserHttpApiController {
 
+  @Authenticated
   @RequestMapping("/me")
   @ResponseStatus(HttpStatus.OK)
   public ResponseEntity<String> me(@AuthenticationPrincipal String username) {
+    return ResponseEntity.ok(username);
+  }
+
+  @RequestMapping("/{username}")
+  @ResponseStatus(HttpStatus.OK)
+  public ResponseEntity<String> getUser(@PathVariable String username) {
     return ResponseEntity.ok(username);
   }
 }
