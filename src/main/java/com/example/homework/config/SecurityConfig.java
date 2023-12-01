f39d3e3c81ced31937dad15aba983471212fd660
dygma0 <404err@naver.com>
2023-12-01 20:54:42 +0900

    refactor: security config 리팩토링

--- src/main/java/com/example/homework/config/SecurityConfig.java
+++ src/main/java/com/example/homework/config/SecurityConfig.java
@@ -2,41 +2,42 @@ package com.example.homework.config;
 
 import com.example.homework.auth.JwtAuthenticationFilter;
 import com.example.homework.auth.JwtAuthenticationProvider;
-import com.example.homework.auth.JwtUtil;
+import com.example.homework.auth.JwtProvider;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
+import org.springframework.security.config.annotation.web.configurers.FormLoginConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableMethodSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
 
   private final AuthenticationConfiguration authenticationConfiguration;
-
-  public SecurityConfig(AuthenticationConfiguration authenticationConfiguration) {
-    this.authenticationConfiguration = authenticationConfiguration;
-  }
+  private final JwtProvider jwtProvider;
 
   @Bean
   public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
     ProviderManager authenticationManager =
         (ProviderManager) authenticationConfiguration.getAuthenticationManager();
-    authenticationManager.getProviders()
-        .add(new JwtAuthenticationProvider(new JwtUtil("secret", 1000 * 60 * 60 * 24)));
+    authenticationManager.getProviders().add(new JwtAuthenticationProvider(jwtProvider));
 
-    return http
-        .addFilterBefore(new JwtAuthenticationFilter(authenticationManager),
+    return http.addFilterBefore(
+            new JwtAuthenticationFilter(authenticationManager),
             UsernamePasswordAuthenticationFilter.class)
-        .authorizeHttpRequests(authorize ->
-            authorize
-                .requestMatchers("/api/**").authenticated()
-                .anyRequest().permitAll()
-        )
+        .formLogin(FormLoginConfigurer::disable)
+        .csrf(CsrfConfigurer::disable)
+        .sessionManagement(
+            sessionConfig -> sessionConfig.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+        .authorizeHttpRequests(authorize -> authorize.anyRequest().permitAll())
         .build();
   }
 }
